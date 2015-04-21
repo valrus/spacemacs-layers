ec8495646182b9cd9f301f368c0636d1f385019a
valrus <imccowan@gmail.com>
2015-04-21 11:51:58 -0700

    Move persp-mode keybindings into keybindings.el.

---
+++ persp-mode/keybindings.el
@@ -0,0 +1,10 @@
+(evil-leader/set-key
+  "Ps" 'persp-switch
+  "Pr" 'persp-rename
+  "Pt" 'persp-temporarily-display-buffer
+  "Pa" 'persp-add-buffer
+  "Pk" 'persp-remove-buffer
+  "Pp" 'persp-prev
+  "Pn" 'persp-next
+  "Pw" 'persp-save-state-to-file
+  "Pl" 'persp-load-state-from-file)
--- persp-mode/packages.el
+++ persp-mode/packages.el
@@ -31,14 +31,4 @@ which require an initialization must be listed explicitly in the list.")
                                  (setq persp-set-last-persp-for-new-frames nil)
                                  (persp-mode 1)
                                  (persp-load-state-from-file "work")
-                                 ))
-  (evil-leader/set-key
-    "Ps" 'persp-switch
-    "Pr" 'persp-rename
-    "Pt" 'persp-temporarily-display-buffer
-    "Pa" 'persp-add-buffer
-    "Pk" 'persp-remove-buffer
-    "Pp" 'persp-prev
-    "Pn" 'persp-next
-    "Pw" 'persp-save-state-to-file
-    "Pl" 'persp-load-state-from-file))
+                                 )))
