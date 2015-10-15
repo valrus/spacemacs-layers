03ab7afdf23f392e7542c6f10907a946814a4709
valrus <imccowan@gmail.com>
2015-10-15 10:45:50 -0700

    Actually clean up persp-mode this time.

--- persp-mode/packages.el
+++ persp-mode/packages.el
@@ -21,14 +21,43 @@ which require an initialization must be listed explicitly in the list.")
   "List of packages to exclude.")
 
 (defun persp-mode/init-persp-mode ()
-  (setq wg-morph-on nil)
-  (add-hook 'after-init-hook #'(lambda ()
-                                 (setq persp-auto-save-opt 2)
-                                 (setq persp-auto-save-num-of-backups 1)
-                                 (setq persp-save-dir (concat (getenv "HOME") "/.emacs.d/private/persp-confs/"))
-                                 (setq persp-auto-save-fname "work")
-                                 (setq persp-auto-resume-time 0)
-                                 (setq persp-set-last-persp-for-new-frames nil)
-                                 (persp-mode 1)
-                                 (persp-load-state-from-file "work")
-                                 )))
+  (use-package persp-mode
+    :commands (
+               persp-switch
+               persp-rename
+               persp-temporarily-display-buffer
+               persp-add-buffer
+               persp-remove-buffer
+               persp-prev
+               persp-next
+               persp-save-state-to-file
+               persp-load-state-from-file
+               )
+    :init
+    (progn
+      (setq wg-morph-on nil)
+      (setq persp-auto-save-opt 2)
+      (setq persp-auto-save-num-of-backups 1)
+      (setq persp-save-dir (concat (getenv "HOME") "/.emacs.d/private/persp-confs/"))
+      (setq persp-auto-save-fname "work")
+      (setq persp-auto-resume-time 0)
+      (setq persp-set-last-persp-for-new-frames nil)
+      (add-hook 'kill-emacs-hook 'persp-save-state-to-file)
+
+      (spacemacs/declare-prefix "L" "layouts")
+      (evil-leader/set-key
+        "La" 'persp-add-buffer
+        "Lk" 'persp-remove-buffer
+        "Ls" 'persp-switch
+        "Lr" 'persp-rename
+        "Lt" 'persp-temporarily-display-buffer
+        "Lp" 'persp-prev
+        "Ln" 'persp-next
+        "Lw" 'persp-save-state-to-file
+        "Ll" 'persp-load-state-from-file
+        "Lc" 'persp-kill))
+    :config
+    (progn
+      (persp-mode 1)
+      (persp-load-state-from-file "work"))))
+
