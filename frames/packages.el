a1469db4af52263369ec139e9c4c9938524c9e2a
valrus <imccowan@gmail.com>
2016-12-17 21:24:36 -0800

    Fix spacemacs complaints about package loading

--- frames/packages.el
+++ frames/packages.el
@@ -1,8 +1,8 @@
 (defvar frames-packages
   '(
     autofit-frame
-    (frame-cmds :toggle t)
-    (window+ :toggle t)
+    frame-cmds
+    window+
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -12,6 +12,12 @@ which require an initialization must be listed explicitly in the list.")
   (setq fit-frame-min-height 40)
   (add-hook 'after-make-frame-functions 'fit-frame)
   (add-hook 'temp-buffer-show-hook 'fit-frame-if-one-window 'append))
+
+(defun frames/init-frame-cmds ()
+  (use-package autofit-frame))
+
+(defun frames/init-window+ ()
+  (use-package window+))
 ;;
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
